2924dfe18eddfc2c5499c41d5d0ca159498d7e10
cu-m <lolc7089@gmail.com>
2022-10-01 21:56:33 +0400

    test

---
+++ Start.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import net.minecraft.client.main.Main;
+
+// 
+// Decompiled by Procyon v0.5.30
+// 
+
+public class Start
+{
+    public static void main(final String[] args) {
+        original(args);
+    }
+    
+    private static void original(final String[] args) {
+        Main.main(concat(new String[] { "--version", "1.8-OptiFine_HD_U_H6", "--accessToken", "0", "--assetsDir", "assets", "--assetIndex", "1.8", "--userProperties", "{}" }, args));
+    }
+    
+    public static <T> T[] concat(final T[] first, final T[] second) {
+        final T[] result = Arrays.copyOf(first, first.length + second.length);
+        System.arraycopy(second, 0, result, first.length, second.length);
+        return result;
+    }
+}
